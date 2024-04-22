04e0d7e72b95fa5d6384c8444e1f6ccbe378917e
Jatesh Parikh <jateshparikh97@gmail.com>
2024-04-22 21:18:41 +0530

    create post page completed

---
+++ app/r/[id]/create/page.tsx
@@ -0,0 +1,147 @@
+"use client";
+
+import { Card, CardFooter, CardHeader } from "@/components/ui/card";
+import Image from "next/image";
+import pfp from "@/public/pfp.png";
+import { Separator } from "@/components/ui/separator";
+import Link from "next/link";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Text, Video } from "lucide-react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { TipTapEditor } from "@/app/components/TipTapEditor";
+import { SubmitButton } from "@/app/components/SubmitButtons";
+import { UploadDropzone } from "@/app/components/UploadThing";
+import { useState } from "react";
+import { CreatePost } from "@/app/actions";
+import { JSONContent } from "@tiptap/react";
+
+const rules = [
+  {
+    id: 1,
+    text: "Remember the human",
+  },
+  {
+    id: 2,
+    text: "Behave like you would in real life",
+  },
+  {
+    id: 3,
+    text: "Look for the original source of content",
+  },
+  {
+    id: 4,
+    text: "Search for duplication before posting",
+  },
+  {
+    id: 5,
+    text: "Read the community guidlines",
+  },
+];
+
+export default function CreatePostRoute({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const [json, setJson] = useState<JSONContent | null>(null);
+  const [title, setTitle] = useState<string | null>(null);
+
+  const createPostReddit = CreatePost.bind(null, { jsonContent: json });
+  return (
+    <div className="max-w-[1000px] mx-auto flex gap-x-10 mt-4">
+      <div className="w-[65%] flex flex-col gap-y-5">
+        <h1 className="font-semibold">
+          Subreddit:{" "}
+          <Link href={`/r/${params.id}`} className="text-primary">
+            r/{params.id}
+          </Link>
+        </h1>
+        <Tabs defaultValue="post" className="w-full">
+          <TabsList className="w-full grid grid-cols-2">
+            <TabsTrigger value="post">
+              <Text className="h-4 w-4 mr-2" /> Post
+            </TabsTrigger>
+            <TabsTrigger value="image">
+              <Video className="h-4 w-4 mr-2" /> Image & Video
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="post">
+            <Card>
+              <form action={createPostReddit}>
+                <input
+                  type="hidden"
+                  name="imageUrl"
+                  value={imageUrl ?? undefined}
+                />
+                <input type="hidden" name="subName" value={params.id} />
+                <CardHeader>
+                  <Label className="ml-2">Title</Label>
+                  <Input
+                    required
+                    name="title"
+                    placeholder="Title"
+                    value={title ?? undefined}
+                    onChange={(e) => setTitle(e.target.value)}
+                  />
+
+                  <TipTapEditor json={json} setJson={setJson} />
+                </CardHeader>
+                <CardFooter>
+                  <SubmitButton text="Create Post" />
+                </CardFooter>
+              </form>
+            </Card>
+          </TabsContent>
+          <TabsContent value="image">
+            <Card>
+              <CardHeader>
+                {imageUrl === null ? (
+                  <UploadDropzone
+                    className="ut-button:bg-primary ut-button:ut-readying:bg-primary/50 ut-label:text-primary ut-button:ut-uploading:bg-primary/50 ut-button:ut-uploading:after:bg-primary"
+                    endpoint="imageUploader"
+                    onClientUploadComplete={(res) => {
+                      setImageUrl(res[0].url);
+                    }}
+                    onUploadError={(error: Error) => {
+                      alert("Error");
+                    }}
+                  />
+                ) : (
+                  <Image
+                    src={imageUrl}
+                    alt="uploaded image"
+                    width={500}
+                    height={400}
+                    className="h-80 rounded-lg w-full object-contain"
+                  />
+                )}
+              </CardHeader>
+            </Card>
+          </TabsContent>
+        </Tabs>
+      </div>
+      <div className="w-[35%]">
+        <Card className="flex flex-col p-4">
+          <div className="flex items-center gap-x-2">
+            <Image className="h-8 w-8" src={pfp} alt="pfp" />
+            <h1 className="font-medium">Posting to Reddit</h1>
+          </div>
+          <Separator className="mt-2" />
+
+          <div className="flex flex-col gap-y-5 mt-5">
+            {rules.map((item) => (
+              <div key={item.id}>
+                <p className="text-sm font-medium">
+                  {item.id}. {item.text}
+                </p>
+                <Separator className="my-1" />
+              </div>
+            ))}
+          </div>
+        </Card>
+      </div>
+    </div>
+  );
+}
